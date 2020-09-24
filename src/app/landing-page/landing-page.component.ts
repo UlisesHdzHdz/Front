7226f1d08d455c1ce6b2c289967a6df2757c1474
Ulises191244 <191244@ids.upchiapas.edu.mx>
2020-09-23 20:48:44 -0500

    add httpcliente and create funtion get request

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscriber } from 'rxjs';
+import { ServiceService } from '../service.service';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+///Clase prinsipal del componente del Logica de negocio
 
-  constructor() { }
+export class LandingPageComponent implements OnInit {
+products = [];
+
+  constructor(private serviceService : ServiceService) { }
 
   ngOnInit(): void {
+    this.serviceService.getProduct("products/").subscribe((data : any []) =>{
+      console.log(data);
+     this.products = data;
+
+      
+    });
+ 
   }
 
 }
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient}  from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  
+  private RES_API_SERVER = "http://localhost:3000/";
 
-  constructor() { }
+  constructor(private httpclient : HttpClient) { }
+
+  public getProduct(nameEndPoint : string){
+    return this.httpclient.get(this.RES_API_SERVER + nameEndPoint);
+  }
+
 }
