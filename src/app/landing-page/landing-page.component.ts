04b3310a9d3fb131a60b4bfeaed3bb5dd811c69d
Ulises191244 <191244@ids.upchiapas.edu.mx>
2020-09-29 10:56:52 -0500

    add a navbar and header component and product table and modify it in css LandinPage

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,3 +1,4 @@
+import { renderFlagCheckIfStmt } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { Subscriber } from 'rxjs';
 import { ServiceService } from '../service.service';
@@ -9,18 +10,38 @@ import { ServiceService } from '../service.service';
 ///Clase prinsipal del componente del Logica de negocio
 
 export class LandingPageComponent implements OnInit {
-products = [];
+  // nombre : tipo = valor
+  //  status : boolean = false;
+  products = [];
+  status = false;
+  nameButton: string = 'Mostrar'
+  info = "No Hay nada"
+ 
 
-  constructor(private serviceService : ServiceService) { }
+  constructor(private serviceService: ServiceService) { }
 
   ngOnInit(): void {
-    this.serviceService.getProduct("products/").subscribe((data : any []) =>{
+ this.serviceService.getProduct("products/").subscribe((data: any[]) => {
       console.log(data);
-     this.products = data;
-
-      
+      this.products = data;
     });
- 
+  }
+
+  sendService() {
+    /*  this.info= 'si hay datos' */
+   
+
+  }
+  clearService() {
+    this.products = [];
+  }
+  showHide(){
+    this.status =!this.status //toggle
+    if(this.status){
+      this.nameButton = 'Ocultar';
+    }else{
+      this.nameButton = 'Mostrar';
+    }
   }
 
 }
